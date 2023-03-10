9bdecda92616244857eb2842d441323914c6850d
Matthew Baker <github@bakermatt.com>
2023-03-10 12:30:05 -0500

    Set up handleNotification API

--- crowdplaysdk/Classes/entry.swift
+++ crowdplaysdk/Classes/entry.swift
@@ -6,6 +6,7 @@ import FlutterPluginRegistrant
 public class CrowdplaySdk {
     public static let shared = CrowdplaySdk()
     lazy var flutterEngine = FlutterEngine(name: "CrowdPlay Flutter engine")
+    private var apiKeyChannel: FlutterMethodChannel?
     var flutterViewController: FlutterViewController?
     private var apiKey = ""
 
@@ -16,8 +17,8 @@ public class CrowdplaySdk {
         
         flutterEngine.run();
         
-        let apiKeyChannel = FlutterMethodChannel(name: "crowdplay.flutter", binaryMessenger: flutterEngine.binaryMessenger)
-        apiKeyChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+        apiKeyChannel = FlutterMethodChannel(name: "crowdplay.flutter", binaryMessenger: flutterEngine.binaryMessenger)
+        apiKeyChannel!.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
             if call.method == "initialization" {
               result(self.apiKey)
             } else if call.method == "CLOSE_CROWDPLAY" {
@@ -40,6 +41,20 @@ public class CrowdplaySdk {
 
     public func presentCrowdplay(vc: UIViewController) {
         vc.present(viewController(), animated: true, completion: nil)
+    }
+
+    public func handleNotification(userInfo: [AnyHashable : Any]?) -> Bool {
+        if userInfo == nil || apiKeyChannel == nil {
+            return false
+        }
+
+        if userInfo!["source"] == nil || userInfo!["source"] as? String == nil || userInfo!["source"] as! String != "crowdplay" {
+            return false
+        }
+
+        apiKeyChannel!.invokeMethod("handleNotification", arguments: userInfo);
+
+        return true
     }
 }
 
