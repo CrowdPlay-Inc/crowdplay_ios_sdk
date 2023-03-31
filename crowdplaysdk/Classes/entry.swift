17b9b9ad1731607cd4b6a25b32732d54dabcd9e1
Matthew Baker <github@bakermatt.com>
2023-03-31 16:26:01 -0400

    * App icons * Add hook for apns environment

--- crowdplaysdk/Classes/entry.swift
+++ crowdplaysdk/Classes/entry.swift
@@ -3,6 +3,44 @@ import Flutter
 // Used to connect plugins (only if you have plugins with iOS platform code).
 import FlutterPluginRegistrant
 
+private extension UIDevice {
+    enum PushEnvironment: String {
+        case unknown
+        case development
+        case production
+    }
+
+    var pushEnvironment: PushEnvironment {
+        guard let provisioningProfile = try? provisioningProfile(),
+              let entitlements = provisioningProfile?["Entitlements"] as? [String: Any],
+              let environment = entitlements["aps-environment"] as? String
+        else {
+            return .unknown
+        }
+
+        return PushEnvironment(rawValue: environment) ?? .unknown
+    }
+
+    // MARK: - Private
+
+    private func provisioningProfile() throws -> [String: Any]? {
+        guard let url = Bundle.main.url(forResource: "embedded", withExtension: "mobileprovision") else {
+            return nil
+        }
+
+        let binaryString = try String(contentsOf: url, encoding: .isoLatin1)
+
+        let scanner = Scanner(string: binaryString)
+        guard scanner.scanUpToString("<plist") != nil, let plistString = scanner.scanUpToString("</plist>"),
+              let data = (plistString + "</plist>").data(using: .isoLatin1)
+        else {
+            return nil
+        }
+
+        return try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]
+    }
+}
+
 public class CrowdplaySdk {
     public static let shared = CrowdplaySdk()
     lazy var flutterEngine = FlutterEngine(name: "CrowdPlay Flutter engine")
@@ -26,6 +64,8 @@ public class CrowdplaySdk {
                 self.flutterViewController?.dismiss(animated: true);
             } else if call.method == "getNativeNotificationToken" {
                 result(self.notificationToken);
+            } else if call.method == "getApnsMode" {
+                result(UIDevice.current.pushEnvironment == .development ? "development" : "production");
             }
         }
         // Used to connect plugins (only if you have plugins with iOS platform code).
