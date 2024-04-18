8cd42198eed1abab63f4df3e2b72ce6bfb499c40
Matthew Baker <github@bakermatt.com>
2024-04-18 11:50:45 -0400

    Start enabling SPM

---
+++ Example_SPM/crowdplaysdk/AppDelegate.swift
@@ -0,0 +1,65 @@
+//
+//  AppDelegate.swift
+//  crowdplaysdk
+//
+//  Created by Matthew Baker on 02/09/2022.
+//  Copyright (c) 2022 Matthew Baker. All rights reserved.
+//
+
+import UIKit
+import crowdplaysdk
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().delegate = self;
+        
+        // Override point for customization after application launch.
+        CrowdplaySdk.shared.initialize(apiKey: "efebd8e4-d1ba-4ba4-86ee-a7a9fe49685e")
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        CrowdplaySdk.shared.setNotificationToken(deviceToken: deviceToken)
+    }
+    
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        //OnTap Notification
+        let userInfo = response.notification.request.content.userInfo
+        print("userNotificationCenter center: response: completionHandler:");
+        print(userInfo);
+        if (self.window?.rootViewController != nil && CrowdplaySdk.shared.handleNotification(userInfo: userInfo, vc: self.window!.rootViewController!)) {
+            print("Handled by Crowdplay");
+            completionHandler();
+        } else {
+            completionHandler();
+        }
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,158 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "crowdplay_ios_sdk",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "crowdplay_ios_sdk",
+            targets: ["crowdplay_ios_sdk", "App", "audioplayers_darwin", "CocoaAsyncSocket", "device_info_plus", "firebase_auth", "firebase_core", "firebase_database", "firebase_messaging", "firebase_storage", "Flutter", "flutter_inappwebview", "flutter_local_notifications", "GTMSessionFetcher", "HTTPParserC", "image_picker_ios", "integration_test", "leveldb", "location", "MTBBarcodeScanner", "OrderedSet", "package_info", "path_provider_foundation", "patrol", "qr_code_scanner", "RecaptchaInterop", "share", "store_redirect", "Telegraph", "uni_links", "url_launcher_ios", "vibration"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.22.0")
+        .package(url: "https://github.com/google/GoogleUtilities.git", from: "7.11.")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "crowdplay_ios_sdk",
+            dependencies: [
+                .product(name: "FirebaseAuth", package: "Firebase")
+                .product(name: "FirebaseStorage", package: "Firebase")
+                .product(name: "FirebaseDatabase", package: "Firebase")
+                .product(name: "FirebaseMessaging", package: "Firebase")
+            ]),
+        .binaryTarget(
+            name: "App",
+            path: "Frameworks/App.xcframework"
+        ),
+        .binaryTarget(
+            name: "audioplayers_darwin",
+            path: "Frameworks/audioplayers_darwin.xcframework"
+        ),
+        .binaryTarget(
+            name: "CocoaAsyncSocket",
+            path: "Frameworks/CocoaAsyncSocket.xcframework"
+        ),
+        .binaryTarget(
+            name: "device_info_plus",
+            path: "Frameworks/device_info_plus.xcframework"
+        ),
+        .binaryTarget(
+            name: "firebase_auth",
+            path: "Frameworks/firebase_auth.xcframework"
+        ),
+        .binaryTarget(
+            name: "firebase_core",
+            path: "Frameworks/firebase_core.xcframework"
+        ),
+        .binaryTarget(
+            name: "firebase_database",
+            path: "Frameworks/firebase_database.xcframework"
+        ),
+        .binaryTarget(
+            name: "firebase_messaging",
+            path: "Frameworks/firebase_messaging.xcframework"
+        ),
+        .binaryTarget(
+            name: "firebase_storage",
+            path: "Frameworks/firebase_storage.xcframework"
+        ),
+        .binaryTarget(
+            name: "Flutter",
+            path: "Frameworks/Flutter.xcframework"
+        ),
+        .binaryTarget(
+            name: "flutter_inappwebview",
+            path: "Frameworks/flutter_inappwebview.xcframework"
+        ),
+        .binaryTarget(
+            name: "flutter_local_notifications",
+            path: "Frameworks/flutter_local_notifications.xcframework"
+        ),
+        .binaryTarget(
+            name: "GTMSessionFetcher",
+            path: "Frameworks/GTMSessionFetcher.xcframework"
+        ),
+        .binaryTarget(
+            name: "HTTPParserC",
+            path: "Frameworks/HTTPParserC.xcframework"
+        ),
+        .binaryTarget(
+            name: "image_picker_ios",
+            path: "Frameworks/image_picker_ios.xcframework"
+        ),
+        .binaryTarget(
+            name: "integration_test",
+            path: "Frameworks/integration_test.xcframework"
+        ),
+        .binaryTarget(
+            name: "leveldb",
+            path: "Frameworks/leveldb.xcframework"
+        ),
+        .binaryTarget(
+            name: "location",
+            path: "Frameworks/location.xcframework"
+        ),
+        .binaryTarget(
+            name: "MTBBarcodeScanner",
+            path: "Frameworks/MTBBarcodeScanner.xcframework"
+        ),
+        .binaryTarget(
+            name: "OrderedSet",
+            path: "Frameworks/OrderedSet.xcframework"
+        ),
+        .binaryTarget(
+            name: "package_info",
+            path: "Frameworks/package_info.xcframework"
+        ),
+        .binaryTarget(
+            name: "path_provider_foundation",
+            path: "Frameworks/path_provider_foundation.xcframework"
+        ),
+        .binaryTarget(
+            name: "patrol",
+            path: "Frameworks/patrol.xcframework"
+        ),
+        .binaryTarget(
+            name: "qr_code_scanner",
+            path: "Frameworks/qr_code_scanner.xcframework"
+        ),
+        .binaryTarget(
+            name: "RecaptchaInterop",
+            path: "Frameworks/RecaptchaInterop.xcframework"
+        ),
+        .binaryTarget(
+            name: "share",
+            path: "Frameworks/share.xcframework"
+        ),
+        .binaryTarget(
+            name: "store_redirect",
+            path: "Frameworks/store_redirect.xcframework"
+        ),
+        .binaryTarget(
+            name: "Telegraph",
+            path: "Frameworks/Telegraph.xcframework"
+        ),
+        .binaryTarget(
+            name: "uni_links",
+            path: "Frameworks/uni_links.xcframework"
+        ),
+        .binaryTarget(
+            name: "url_launcher_ios",
+            path: "Frameworks/url_launcher_ios.xcframework"
+        ),
+        .binaryTarget(
+            name: "vibration",
+            path: "Frameworks/vibration.xcframework"
+        ),
+        .testTarget(
+            name: "crowdplay_ios_sdkTests",
+            dependencies: ["crowdplay_ios_sdk"]),
+    ]
+)
