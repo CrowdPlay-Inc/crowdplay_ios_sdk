9e8d06b2bf21d92bc8516689d2c6acaafbabf6e7
Matthew Baker <github@bakermatt.com>
2024-04-18 13:11:16 -0400

    Move target references to be a dependency of the primary target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "crowdplay_ios_sdk",
-            targets: ["crowdplay_ios_sdk", "App", "audioplayers_darwin", "CocoaAsyncSocket", "device_info_plus", "firebase_auth", "firebase_core", "firebase_database", "firebase_messaging", "firebase_storage", "Flutter", "flutter_inappwebview", "flutter_local_notifications", "GTMSessionFetcher", "HTTPParserC", "image_picker_ios", "integration_test", "location", "MTBBarcodeScanner", "OrderedSet", "package_info", "path_provider_foundation", "patrol", "qr_code_scanner", "share", "store_redirect", "Telegraph", "uni_links", "url_launcher_ios", "vibration"]),
+            targets: ["crowdplay_ios_sdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,7 +25,8 @@ let package = Package(
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                "App", "audioplayers_darwin", "CocoaAsyncSocket", "device_info_plus", "firebase_auth", "firebase_core", "firebase_database", "firebase_messaging", "firebase_storage", "Flutter", "flutter_inappwebview", "flutter_local_notifications", "GTMSessionFetcher", "HTTPParserC", "image_picker_ios", "integration_test", "location", "MTBBarcodeScanner", "OrderedSet", "package_info", "path_provider_foundation", "patrol", "qr_code_scanner", "share", "store_redirect", "Telegraph", "uni_links", "url_launcher_ios", "vibration"
             ]),
         .binaryTarget(
             name: "App",
